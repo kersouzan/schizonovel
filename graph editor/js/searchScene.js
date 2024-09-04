9b1556b2b0f5fd45a25c77b75f059cd409de21b5
kersouzan <kersouzan@gmail.com>
2024-09-04 21:19:44 +0200

    up graph exemple

--- graph editor/js/searchScene.js
+++ graph editor/js/searchScene.js
@@ -2,13 +2,13 @@ function searchScene() {
     var label = document.getElementsByName("search")[0].value;
     
     if(label.startsWith("bool_") && boolGraph[label] != null) {
-        var coord = boolGraph[label].htmlCoordinates.split(" ")[0];
-        window.scrollTo(0, coord);
+        var coord = boolGraph[label].htmlCoordinates.split(" ");
+        window.scrollTo(coord[1], coord[0]);
         return;
     }
     if(label.startsWith("scene_") && sceneGraph[label] != null) {
-        var coord = sceneGraph[label].htmlCoordinates.split(" ")[0];
-        window.scrollTo(0, coord);
+        var coord = sceneGraph[label].htmlCoordinates.split(" ");
+        window.scrollTo(coord[1], coord[0]);
         return;
     }
     displayConsole("Impossible de trouver ce label.");
