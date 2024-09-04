b77b2889e1abd5bd5d9c25af9d76575a7f1ea6a0
kersouzan <kersouzan@gmail.com>
2024-09-04 20:34:47 +0200

    add label search

--- graph editor/js/searchScene.js
+++ graph editor/js/searchScene.js
@@ -1,3 +1,15 @@
 function searchScene() {
-    alert("En cours");
+    var label = document.getElementsByName("search")[0].value;
+    
+    if(label.startsWith("bool_") && boolGraph[label] != null) {
+        var coord = boolGraph[label].htmlCoordinates.split(" ")[0];
+        window.scrollTo(0, coord);
+        return;
+    }
+    if(label.startsWith("scene_") && sceneGraph[label] != null) {
+        var coord = sceneGraph[label].htmlCoordinates.split(" ")[0];
+        window.scrollTo(0, coord);
+        return;
+    }
+    displayConsole("Impossible de trouver ce label.");
 }
